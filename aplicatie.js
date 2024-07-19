0519eaafc0f9813ec2e03a8c2d8bee4e1dd108d7
iuliapraja <iuliaprajaa@gmail.com>
2024-07-19 13:41:08 +0300

    s

---
+++ aplicatie.js
@@ -0,0 +1,33 @@
+function App() {
+    return (
+      <div>
+          <div class="conversatii">
+            
+            <div class="mesaje">
+                <div class="profil">
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="mesajuser">
+            <div class="container">
+                <p>
+                  Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve &#128512;.
+                </p>
+                <span class="time-right">08:34</span>
+            </div>
+            <div class="container">
+              <p>Agreed, Harry &#128077;.We had some good moments, but we need to be more clinical in front of the goal &#128533;.</p>
+              <span class="time-left">08:34</span>
+            </div>
+            
+            <div class="container darker">
+              <p>We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs.</p>
+              <span class="time-left">08:36</span>
+            </div>
+        </div>
+      </div>
+    );
+  }
