81970ce73fe81d545fcb6386ee666f939adbf7f6
iuliapraja <iuliaprajaa@gmail.com>
2024-07-19 14:21:24 +0300

    s

--- aplicatie.js
+++ aplicatie.js
@@ -37,17 +37,8 @@ const MesajTrimis = ( props ) => {
 const Mesajuser = () => {
     return (
         <div class="mesajuser">
-            <div class="container">
-                <p>
-                  Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve &#128512;.
-                </p>
-                <span class="time-right">08:34</span>
-            </div>
-            <div class="container">
-              <p>Agreed, Harry &#128077;.We had some good moments, but we need to be more clinical in front of the goal &#128533;.</p>
-              <span class="time-left">08:34</span>
-            </div>
-            
+            <MesajPrimit ora={"08:34"} mesaj={"Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve &#128512;."} />
+            <MesajPrimit ora={"08:35"} mesaj={"Agreed, Harry &#128077;.We had some good moments, but we need to be more clinical in front of the goal &#128533;"} />
             <MesajTrimis ora={"08:36"} mesaj={"We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs."}/>
             <MesajTrimis ora={"08:38"} mesaj={"da da"}/>
         </div>
