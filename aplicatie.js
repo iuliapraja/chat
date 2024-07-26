03251d5f3d1980c971b57d149c8c87c0802da132
iuliapraja <iuliaprajaa@gmail.com>
2024-07-26 12:46:03 +0300

    s

--- aplicatie.js
+++ aplicatie.js
@@ -10,7 +10,7 @@ const Top = () => {
 
 const ListaMesaje = () => {
     return(
-        <div>
+        <div class="center">
             <MesajPrimit ora={"08:34"} mesaj={"Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve 🙂"} />
             <MesajPrimit ora={"08:35"} mesaj={"Agreed, Harry 👍.We had some good moments, but we need to be more clinical in front of the goal 👀."} />
             <MesajTrimis ora={"08:36"} mesaj={"We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs."}/>
