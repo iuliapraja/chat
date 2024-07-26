4cf37a845127f87e3a76ed75c428205d89c81557
iuliapraja <iuliaprajaa@gmail.com>
2024-07-26 12:38:02 +0300

    s

--- aplicatie.js
+++ aplicatie.js
@@ -1,22 +1,3 @@
-
-const Mesajuser = () => {
-    return (    
-        <div class="mesajuser">
-
-
-            <Top/>
-            2.listamesaje
-            <MesajPrimit ora={"08:34"} mesaj={"Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve 🙂"} />
-            <MesajPrimit ora={"08:35"} mesaj={"Agreed, Harry 👍.We had some good moments, but we need to be more clinical in front of the goal 👀."} />
-            <MesajTrimis ora={"08:36"} mesaj={"We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs."}/>
-            <MesajTrimis ora={"08:38"} mesaj={"da da"}/>
-            3.Trimite
-
-        </div>
-    )
-
-}
-
 const Top = () => {
     return (
         <div class="top">
@@ -26,6 +7,32 @@ const Top = () => {
         </div>
     )
 }
+
+const ListaMesaje = () => {
+    return(
+        <div>
+            <MesajPrimit ora={"08:34"} mesaj={"Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve 🙂"} />
+            <MesajPrimit ora={"08:35"} mesaj={"Agreed, Harry 👍.We had some good moments, but we need to be more clinical in front of the goal 👀."} />
+            <MesajTrimis ora={"08:36"} mesaj={"We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs."}/>
+            <MesajTrimis ora={"08:38"} mesaj={"da da"}/>
+        </div>
+    )
+}
+const Mesajuser = () => {
+    return (    
+        <div class="mesajuser">
+
+
+            <Top/>
+            <ListaMesaje/>
+            3.Trimite
+
+        </div>
+    )
+
+}
+
+
 
 const Conversatii = () => {
     return (
