036808d39db26a6d29a7efcb3300b5c63716c22a
iuliapraja <iuliaprajaa@gmail.com>
2024-07-26 11:09:28 +0300

    s

--- aplicatie.js
+++ aplicatie.js
@@ -1,20 +1,27 @@
 
+const Mesajuser = () => {
+    return (    
+        <div class="mesajuser">
+            <MesajPrimit ora={"08:34"} mesaj={"Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve 🙂"} />
+            <MesajPrimit ora={"08:35"} mesaj={"Agreed, Harry 👍.We had some good moments, but we need to be more clinical in front of the goal 👀."} />
+            <MesajTrimis ora={"08:36"} mesaj={"We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs."}/>
+            <MesajTrimis ora={"08:38"} mesaj={"da da"}/>
+        </div>
+    )
+
+}
+
 const Conversatii = () => {
     return (
         <div class="conversatii">
-            
+                    
             <div class="mesaje">
                 <div class="profil">
                 <img src="https://i.pravatar.cc/70" alt="Avatar"></img>
                 Iulia Praja
                 </div>
             </div>
-            <div class="mesajuser">
-            <MesajPrimit ora={"08:34"} mesaj={"Hey lads, tough game yesterday. Let's talk about what went wrong and how we can improve 🙂"} />
-            <MesajPrimit ora={"08:35"} mesaj={"Agreed, Harry 👍.We had some good moments, but we need to be more clinical in front of the goal 👀."} />
-            <MesajTrimis ora={"08:36"} mesaj={"We need to control the midfield and exploit their defensive weaknesses. Bruno and Paul, I'm counting on your creativity.Marcus and Jadon, stretch their defense wide.Use your pace and take on their full-backs."}/>
-            <MesajTrimis ora={"08:38"} mesaj={"da da"}/>
-        </div>
+            <Mesajuser />
         </div>
     )
 }
@@ -48,7 +55,7 @@ function App() {
       <div>
 
         <Conversatii />
-        <Mesajuser />
+
 
       </div>
     );
