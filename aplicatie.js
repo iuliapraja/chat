124d40e2b6a1cdcfdb476701ef6330da9925dfe4
iuliapraja <iuliaprajaa@gmail.com>
2024-07-30 13:20:28 +0300

    s

--- aplicatie.js
+++ aplicatie.js
@@ -1,6 +1,6 @@
 const Avatar = (props) => {
     return (
-        <div class="avatar">
+        <div class="avatar" style={"background-image: url(" + props.src + ")"}>
             <img src={props.src} alt="Avatar"></img>
         </div>
     )
